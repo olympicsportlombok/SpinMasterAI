740f12e53b6a83691b2fcb24e18a038a5fbefcf3
olympicsportlombok <olympicsport.lombok@gmail.com>
2025-11-29 10:41:41 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,16 +2,20 @@ import streamlit as st
 from google import genai
 
 # KONEKSI KE GOOGLE AI STUDIO
-client = genai.Client(api_key="AIzaSyCLMY65VcgaRMyzh7NcvaVzj_s4yWzVI3o")
+client = genai.Client(api_key="ISI_API_KEY_KAMU_DI_SINI")
 
 st.title("SpinMaster AI by Olympic Sport Lombok")
 
 input_user = st.text_area("Masukkan pertanyaan / data:")
 
 if st.button("Kirim"):
-    response = client.models.generate_content(
-        model="gemini-2.0-flash",
-        contents=input_user
-    )
-    st.subheader("Hasil:")
-    st.write(response.text)
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",   # ⬅️ MODEL GRATIS
+            contents=input_user
+        )
+        st.subheader("Hasil:")
+        st.write(response.text)
+
+    except Exception as e:
+        st.error(f"Terjadi error: {e}")
